e1ecfcb30ccbb7978477092f3e6b0e6c15e82313
LeeJaeWan <ljw6398@hanmail.net>
2024-02-25 01:34:31 +0900

    commit & push

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -2,12 +2,13 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository
 {
-    Member save(Member member);
+    Member save(Member member) throws SQLException;
     Optional<Member> findbyId(Long id);
     Optional<Member> findByName(String name);
     List<Member> findAll();
